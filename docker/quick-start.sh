adc6c9844b2f483fa7fb8365c733c6f3705e00ae
Taylor Dahlke <taylor@hivemapper.com>
2021-02-01 11:09:30 -0800

    Update quick-start.sh
    Revamped the quickstart.sh script to forward the X11 GUI to the local display for the case where the docker host is either local or remote.

--- docker/quick-start.sh
+++ docker/quick-start.sh
@@ -1,2 +1,28 @@
-docker pull deepdso/deepdso:centos8
-docker run --gpus all -w /working -v $1:/working -it deepdso/deepdso:centos8;
+####################################################################
+#
+#   USAGE:  
+#       1)  If docker host is your local machine:
+#               ./quick_start.sh ~/my_testing_dir 
+#
+#       2)  If docker host is a remote machine (like an AWS instance):
+#                ./quick_start.sh ~/my_testing_dir remote
+#
+####################################################################
+
+if [ $2 = 'remote' ]; then
+    echo "Setting up for docker host on a remote machine"
+    # These steps are needed to forward the GUI functionality from the docker container ---> remote docker host --> your local display
+    XSOCK=/tmp/.X11-unix
+    XAUTH=/tmp/.docker.xauth
+    xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | sudo xauth -f $XAUTH nmerge -
+    sudo chmod 777 $XAUTH
+    X11PORT=`echo $DISPLAY | sed 's/^[^:]*:\([^\.]\+\).*/\1/'`
+    TCPPORT=`expr 6000 + $X11PORT`
+    sudo ufw allow from 172.17.0.0/16 to any port $TCPPORT proto tcp 
+    DISPLAY=`echo $DISPLAY | sed 's/^[^:]*\(.*\)/172.17.0.1\1/'`
+    docker run --gpus all -w /working  --rm -e DISPLAY=$DISPLAY -v $1:/working -v $XAUTH:$XAUTH -e XAUTHORITY=$XAUTH -it deepdso/deepdso:centos8;
+else
+    # Pull & start docker container
+    docker pull deepdso/deepdso:centos8 # Comment this line if you wish to use your own local build of deepdso/deepdso:centos8
+    docker run --gpus all -w /working -v $1:/working -it deepdso/deepdso:centos8;
+fi
